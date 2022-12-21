42ee491a97854e17f06da18e005d7e8ae484acc5
Arjun Kathuria <arjun.kathuria.ext@juspay.in>
2022-12-21 16:05:22 +0530

    Enables building with newest Stackage snapshot with ghc 9.2.5
    * Adds stack.yaml file that builds the package with newest lts-20.4
      stackage snapshot
    
    * Snapshots includes ghc 9.2.5
    
    * Makes 'ambiguous-fields' a warning instead of a build error
    
    * This was done w.r.t to the Beckn & Euler GHC 9.2.5 upgrade process

--- src/Database/Beam/MySQL/Syntax/Select.hs
+++ src/Database/Beam/MySQL/Syntax/Select.hs
@@ -1,5 +1,7 @@
 -- Due to RDP plugin.
 {-# OPTIONS_GHC -Wno-incomplete-record-updates #-}
+{-# OPTIONS_GHC -Wwarn -Wambiguous-fields #-}
+
 {-# LANGUAGE TypeFamilies #-}
 
 module Database.Beam.MySQL.Syntax.Select where
